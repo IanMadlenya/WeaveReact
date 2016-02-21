72ea7d2818a1d088eba671ccea3270daf464afd9
Sanjay krishna anbalagan <sanjay1909@gmail.com>
2016-02-20 21:14:30 -0500

    UI to Session is automated

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 window.demo ={};
+window.demo.App = App;
 window.demo.init = function (container) {
     ReactDOM.render( <App/> , document.getElementById(container));
 }
