dc3510e539a5459a34682932effa050f62cb51d1
Sanjay Krishna Anbalagan <sanjay1909@gmail.com>
2016-02-26 11:47:10 -0500

    added compoents to expose them

--- src/index.js
+++ src/index.js
@@ -13,18 +13,19 @@ import Navbar from "./components/navbar/Navbar";
 import navbarConfig from "./components/navbar/Config";
 
 import InlineStyle from "./configs/InlineStyle";
+import CSS from "./configs/CSS";
 
 
 import App from "./utils/App";
-
-
-
+import PropsManager from "./components/PropsManager";
 
 
 exports.Tree = Tree;
-exports.Modal = Modal;
 exports.TreeConfig = TreeConfig;
+
+exports.Modal = Modal;
 exports.ModalConfig = ModalConfig;
+
 exports.SplitPane = SplitPane;
 exports.SplitPaneConfig = SplitPaneConfig;
 
@@ -32,6 +33,9 @@ exports.Navbar = Navbar;
 exports.navbarConfig = navbarConfig;
 
 exports.InlineStyle = InlineStyle;
+exports.CSS = CSS;
+
+exports.PropsManager = PropsManager;
 
 
 exports.registerToolImplementation = function (asClassName, jsClass) {
@@ -51,4 +55,12 @@ exports.registerToolConfig = function (toolClass, configClass) {
 
 exports.getToolConfig = function (toolClass) {
     App.getToolConfig(toolClass);
+}
+
+exports.hookSessionStateForComponentChildren = function (ReactChildren,ownerConfig)  {
+    App.hookSessionStateForComponentChildren(ReactChildren,ownerConfig);
+}
+
+exports.renderChildren = function (reactComp,propsManager)  {
+    App.renderChildren(reactComp,propsManager);
 }
